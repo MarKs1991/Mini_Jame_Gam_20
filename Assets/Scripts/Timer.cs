a924958bfc9c8ec801b70f97c3f207b4c48a280c
Kubickeck <adrian.kubicek@web.de>
2023-08-06 18:21:42 +0200

    Added timer functionality

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+
 public class Timer : MonoBehaviour
 {
 
     public float timeRemaining = 10;
     public bool timerIsRunning = false;
+
+    public TextMeshProUGUI timerText = null;
+    private CutsceneBehaviour cB = null;
     private void Start()
     {
         // Starts the timer automatically
+        cB = GetComponent<CutsceneBehaviour>();
         timerIsRunning = true;
     }
     private void FixedUpdate()
     {
-        if (timerIsRunning)
+        if (timerIsRunning && cB.GetInteractableState())
         {
-            if (timeRemaining > 0)
+            timerText.text = timeRemaining.ToString("N0");
+
+            if (timeRemaining > 0.1f)
             {
                 timeRemaining -= Time.deltaTime;
                 //Debug.Log(timeRemaining);
